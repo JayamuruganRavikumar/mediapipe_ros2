d7fd3dd1fbb83cd10b3376b5bd438172132ef72c
openpose1 <ravikumar@igmr.rwth-aachen.de>
2024-05-23 14:00:07 +0200

    adding depth

--- media_pipe_ros2/src/media_pipe_ros2/media_pipe_ros2/pose_detector.py
+++ media_pipe_ros2/src/media_pipe_ros2/media_pipe_ros2/pose_detector.py
@@ -1,6 +1,7 @@
 import rclpy
 import cv2
 import mediapipe as mp
+import numpy as np
 from rclpy.node import Node
 from media_pipe_ros2_msg.msg import  MediaPipeHumanPoseList                            
 from mediapipe.python.solutions.pose import PoseLandmark
@@ -28,7 +29,6 @@ NAME_POSE = [
     (PoseLandmark.RIGHT_HEEL), ( PoseLandmark.LEFT_FOOT_INDEX),
     (PoseLandmark.RIGHT_FOOT_INDEX)
 ]
-#cap = cv2.VideoCapture(0)
 
 class PosePublisher(Node):
 
@@ -36,78 +36,71 @@ class PosePublisher(Node):
         super().__init__('mediapipe_pose_publisher')
         self.publisher_ = self.create_publisher(MediaPipeHumanPoseList, '/mediapipe/human_pose_list', 10)
         self.image_publisher=self.create_publisher(Image, '/processed/image', 10)
-        self.subscription = self.create_subscription(Image, '/rgb_to_depth/image_raw', self.getimage_callback, 10)
-        
+        self.subscription = self.create_subscription(Image, '/rgb_to_depth/image_raw', self.getrgb_callback, 10)
+        self.subscription = self.create_subscription(Image, '/depth/image_raw', self.getdepth_callback, 10)
         self.bridge = CvBridge()
 
-    def getimage_callback(self, msg):
+
+    def compare_depth(self):
+        print("called")
+
         mediapipehumanposelist = MediaPipeHumanPoseList() 
         
-
         with mp_pose.Pose(
                min_detection_confidence=0.5,
                min_tracking_confidence=0.5) as pose:
-            #while cap.isOpened():
+            results = pose.process(self.rgb)
 
-            #    success, image = cap.read()
-            #    if not success:
-            #        print("Sem camera.")
-                            
-            image_msg = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            image = cv2.cvtColor(cv2.flip(image_msg, 0), cv2.COLOR_BGR2RGB)                
-            #image.flags.writeable = False
-            results = pose.process(image)
-            #image.flags.writeable = True
-            #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            #imageHeight, imageWidth, _ = image.shape
-
-                # Draw the pose annotation on the image.
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # Draw the pose annotation on the image.
+            self.rgb.flags.writeable = True
+            image = cv2.cvtColor(self.rgb, cv2.COLOR_RGB2BGR)
             mp_drawing.draw_landmarks( image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
             
             if results.pose_landmarks != None:
-                index_pose = 0
-                for pose_landmarks in (results.pose_landmarks.landmark):
-                    #print(index_pose)
-                    mediapipehumanposelist.human_pose_list[index_pose].name = str(NAME_POSE[index_pose])
-                    mediapipehumanposelist.human_pose_list[index_pose].x = pose_landmarks.x
-                    mediapipehumanposelist.human_pose_list[index_pose].y = pose_landmarks.y
-                    mediapipehumanposelist.human_pose_list[index_pose].visibility = pose_landmarks.visibility
-                    index_pose = index_pose +1
+                ids = 0 
+                #for ids, pose_landmarks enumerate(results.pose_landmarks.landmark):
+                for pose_landmarks in(results.pose_landmarks.landmark):
+                    #if 16 <= ids < 23:
+                    mediapipehumanposelist.human_pose_list[ids].name = str(NAME_POSE[ids])
+                    mediapipehumanposelist.human_pose_list[ids].x = pose_landmarks.x
+                    mediapipehumanposelist.human_pose_list[ids].y = pose_landmarks.y
+                    mediapipehumanposelist.human_pose_list[ids].visibility = pose_landmarks.visibility
+                    ids = ids+1
 
                 mediapipehumanposelist.num_humans = 1
                 self.publisher_.publish(mediapipehumanposelist)
-            else: # responsavel por mandar 0 nos topicos quando corpo nao esta na tela
-                index_pose = 0
+            else: 
+                ids = 0
                 for point in mp_pose.PoseLandmark:                          
                                                                                       
-                    mediapipehumanposelist.human_pose_list[index_pose].name = str(NAME_POSE[index_pose])
-                    mediapipehumanposelist.human_pose_list[index_pose].x = 0.0
-                    mediapipehumanposelist.human_pose_list[index_pose].y = 0.0
-                    mediapipehumanposelist.human_pose_list[index_pose].visibility = 0.0
-                    index_pose = index_pose +1
-
+                    mediapipehumanposelist.human_pose_list[ids].name = str(NAME_POSE[ids])
+                    mediapipehumanposelist.human_pose_list[ids].x = 0.0
+                    mediapipehumanposelist.human_pose_list[ids].y = 0.0
+                    mediapipehumanposelist.human_pose_list[ids].visibility = 0.0
+                    ids = ids+1
             
                 mediapipehumanposelist.num_humans = 1
                 self.publisher_.publish(mediapipehumanposelist)
 
             img_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
             self.image_publisher.publish(img_msg)
-            #cv2.imshow('MediaPipe Pose', image)
+        
+    def getrgb_callback(self, msg):
+        image_msg = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        self.rgb = cv2.cvtColor(cv2.flip(image_msg, 0), cv2.COLOR_BGR2RGB)                
+
+    def getdepth_callback(self, msg):
+        #conver form 32FC1 to np array
+        self.depth = np.frombuffer(msg.data, dtype=np.float32).reshape(512, msg.width)                 
+        self.compare_depth()
 
 
 def main(args=None):
+
     rclpy.init(args=args)
-
     pose_publisher=PosePublisher()
-    #pose_publisher.getimage_callback()    
-    
-   # cap.release()
-    
     rclpy.spin(pose_publisher)
-
     pose_publisher.destroy_node()
     rclpy.shutdown()
 
