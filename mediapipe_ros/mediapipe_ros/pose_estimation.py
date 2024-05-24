3652d514dcf85e0dae2303d99d5592976eacd3e7
openpose1 <ravikumar@igmr.rwth-aachen.de>
2024-05-24 14:03:47 +0200

    using depth to rgb

--- mediapipe_ros/mediapipe_ros/pose_estimation.py
+++ mediapipe_ros/mediapipe_ros/pose_estimation.py
@@ -3,7 +3,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 from rclpy.node import Node
-from media_pipe_ros2_msg.msg import  MediaPipeHumanPoseList                            
+from mediapipe_msg.msg import PoseList
 from mediapipe.python.solutions.pose import PoseLandmark
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -34,14 +34,13 @@ class PosePublisher(Node):
 
     def __init__(self):
         super().__init__('mediapipe_pose_publisher')
-        self.publisher_ = self.create_publisher(MediaPipeHumanPoseList, '/mediapipe/human_pose_list', 10)
+        self.publisher_ = self.create_publisher(PoseList, '/mediapipe/pose_list', 10)
         self.image_publisher=self.create_publisher(Image, '/processed/image', 10)
-        self.subscription = self.create_subscription(Image, '/rgb_to_depth/image_raw', self.getrgb_callback, 10)
-        self.subscription = self.create_subscription(Image, '/depth/image_raw', self.getdepth_callback, 10)
+        self.subscription = self.create_subscription(Image, '/rgb/image_raw', self.getrgb_callback, 10)
+        self.subscription = self.create_subscription(Image, '/depth_to_rgb/image_raw', self.getdepth_callback, 10)
         self.bridge = CvBridge()
 
-
-
+        
     def getdepth_callback(self, msg):
         #conver form 32FC1 to np array
         self.depth = np.frombuffer(msg.data, dtype=np.uint16).reshape(msg.height,msg.width)                 
@@ -54,11 +53,12 @@ class PosePublisher(Node):
 
     def compare_depth(self, image, depth):
 
-        mediapipehumanposelist = MediaPipeHumanPoseList() 
+        poselist = PoseList() 
         
         with mp_pose.Pose(
                min_detection_confidence=0.5,
                min_tracking_confidence=0.5) as pose:
+            image.flags.writeable = False
             results = pose.process(image)
 
             # Draw the pose annotation on the image.
@@ -66,33 +66,32 @@ class PosePublisher(Node):
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             mp_drawing.draw_landmarks( image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
             h, w, c = image.shape
+            
 
             if results.pose_landmarks != None:
                 index = 0
                 for ids, pose_landmarks in enumerate(results.pose_landmarks.landmark):
+                    #check for wrist
                     if 15 <= ids < 23:
                         cx,cy = int(pose_landmarks.x*w), int(pose_landmarks.y*h)
-                        mediapipehumanposelist.human_pose_list[index].name = str(NAME_POSE[ids])
-                        mediapipehumanposelist.human_pose_list[index].x = float(cx)
-                        mediapipehumanposelist.human_pose_list[index].y = float(cy)
-                        mediapipehumanposelist.human_pose_list[index].visibility = pose_landmarks.visibility
-                        print(depth[cx,cy])
+                        poselist.human_pose[index].name = str(NAME_POSE[ids])
+                        poselist.human_pose[index].x = cx
+                        poselist.human_pose[index].y = cy
+                        poselist.human_pose[index].distance = int(depth[cx,cy])
                         index+=1
 
-                mediapipehumanposelist.num_humans = 1
-                self.publisher_.publish(mediapipehumanposelist)
+                self.publisher_.publish(poselist)
             else: 
                 ids = 0
                 for point in mp_pose.PoseLandmark:                          
                     if 15 <= ids < 23:
-                        mediapipehumanposelist.human_pose_list[ids].name = str(NAME_POSE[ids])
-                        mediapipehumanposelist.human_pose_list[ids].x = 0.0
-                        mediapipehumanposelist.human_pose_list[ids].y = 0.0
-                        mediapipehumanposelist.human_pose_list[ids].visibility = 0.0
+                        poselist.human_pose[ids].name = str(NAME_POSE[ids])
+                        poselist.human_pose[ids].x = 0
+                        poselist.human_pose[ids].y = 0
+                        depthlist.human_pose[ids].distance = 0 
                         ids +=1
             
-                mediapipehumanposelist.num_humans = 1
-                self.publisher_.publish(mediapipehumanposelist)
+                self.publisher_.publish(poselist)
 
             img_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
             self.image_publisher.publish(img_msg)
@@ -108,3 +107,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
