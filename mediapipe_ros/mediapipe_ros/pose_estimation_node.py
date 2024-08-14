201baf109399495a77c6d5ce4afb900599c8eb99
Jay <jayamurugancbe@gmail.com>
2024-08-14 21:46:55 +0200

    0.1.0

--- mediapipe_ros/mediapipe_ros/pose_estimation_node.py
+++ mediapipe_ros/mediapipe_ros/pose_estimation_node.py
@@ -13,9 +13,9 @@ import cv2
 import numpy as np
 
 from cv_bridge import CvBridge, CvBridgeError
-
-from sensor_msgs.msg import Image
-from mediapipe_msg.msg import PoseList
+from sensor_msgs.msg import Image, CameraInfo
+from mediapipe_msg.msg import Point2D, Keypoint, PoseArray
+from geometry_msgs.msg import Point
 
 import mediapipe as mp
 from mediapipe.python.solutions.pose import PoseLandmark
@@ -27,10 +27,46 @@ class PosePublisher(LifecycleNode):
 
         super().__init__("pose_estimation_node")
 
-        self.declare_parameter("image_reliability", QoSReliabilityPolicy.BEST_EFFORT)
-        self.enable = self.get_parameter("enable").get_parameter_value().bool_value
-        self.model_path = "model/pose_landmarker_full.task"
-        self.bridge = CvBridge()
+        self.declare_parameter("rgb_image_reliability", QoSReliabilityPolicy.BEST_EFFORT)
+        self.declare_parameter("depth_image_reliability", QoSReliabilityPolicy.BEST_EFFORT)
+        self.declare_parameter("depth_info_reliability", QoSReliabilityPolicy.BEST_EFFORT)
+        self.declare_parameter("enable", True)
+        self.cv_bridge = CvBridge()
+        self.get_logger().info("Pose estimattion Node created")
+
+    def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
+
+        self.get_logger().info(f"Configuring {self.get_name()}")
+        self.enable = self.get_parameter(
+            "enable").get_parameter_value().bool_value
+
+        rgb_reliability = self.get_parameter("rgb_image_reliability").get_parameter_value().integer_value
+        self.rgb_image_qos_profile = QoSProfile(reliability=rgb_reliability,
+                                            history=QoSHistoryPolicy.KEEP_LAST,
+                                            durability=QoSDurabilityPolicy.VOLATILE,
+                                            depth=1)
+
+        depth_reliability = self.get_parameter("depth_image_reliability").get_parameter_value().integer_value
+        self.depth_image_qos_profile = QoSProfile(reliability=depth_reliability,
+                                            history=QoSHistoryPolicy.KEEP_LAST,
+                                            durability=QoSDurabilityPolicy.VOLATILE,
+                                            depth=1)
+
+        depth_info_reliability = self.get_parameter("depth_info_reliability").get_parameter_value().integer_value
+        self.depth_info_qos_profile = QoSProfile(reliability=depth_info_reliability,
+                                            history=QoSHistoryPolicy.KEEP_LAST,
+                                            durability=QoSDurabilityPolicy.VOLATILE,
+                                            depth=1)
+
+        #publishers
+        self._pose_pub = self.create_publisher(PoseArray, "pose_list", 10)
+        self._im_pub = self.create_publisher(Image, "processed", 10)
+
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_activate(self, state: LifecycleState) -> TransitionCallbackReturn:
+
+        self.get_logger().info(f"Activating {self.get_name()}")
         self.pose_name = [
             "NOSE", "LEFT_EYE_INNER",
             "LEFT_EYE", "LEFT_EYE_OUTER",
@@ -50,41 +86,31 @@ class PosePublisher(LifecycleNode):
             "RIGHT_HEEL", "LEFT_FOOT_INDEX",
             "RIGHT_FOOT_INDEX"
         ]
-        self.get_logger().info("Pose estimattion Node created")
-
-    def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
-
-        self.get_logger().info(f"Configuring {self.get_name()}")
-        self.enable = self.get_parameter(
-            "enable").get_parameter_value().bool_value
-        self.reliability = self.get_parameter("image_reliability").get_parameter_value().integer_value
-
-        self.image_qos_profile = QoSProfile(reliability=self.reliability,
-                                            history=QoSHistoryPolicy.KEEP_LAST,
-                                            durability=QoSDurabilityPolicy.VOLATILE,
-                                            depth=1)
-
-        #publishers
-        self._pose_pub = self.create_lifecycle_publisher(PoseList, "pose_list", 10)
-        self._im_pub = self.create_lifecycle_publisher(Image, "processed", 10)
-        self.cv_bridge = CvBridge()
-
-        return TransitionCallbackReturn.SUCCESS
-
-    def on_activate(self, state: LifecycleState) -> TransitionCallbackReturn:
-
-        self.get_logger().info(f"Activating {self.get_name()}")
-        self.mp_drawing = mp.solutions.drawing_utils
         self.mp_pose = mp.solutions.pose
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.get_logger().info("Pose object intialized")
 
         #suscribers
-        self._rgb_sub = self.create_subscription(
-            Image,
-            "image_raw",
-            self.rgb_cb,
-            self.image_qos_profile
+        self._rgb_sub = message_filters.Subscriber(self, 
+            Image, "/mediapipe/rgb/image_raw", qos_profile=self.rgb_image_qos_profile
         )
-        super().on_activate(state)
+        self.get_logger().info("rgb sub created")
+
+        self._depth_sub = message_filters.Subscriber(self, 
+            Image, "/mediapipe/depth_to_rgb/image_raw", qos_profile=self.depth_image_qos_profile
+        )
+        self.get_logger().info("depth sub created")
+
+        self._depth_info_sub = message_filters.Subscriber(self,
+            CameraInfo, "/mediapipe/depth_to_rgb/camera_info", qos_profile=self.depth_info_qos_profile)
+        self.get_logger().info("message sub created")
+
+        self._synchronizer = message_filters.ApproximateTimeSynchronizer(
+                (self._rgb_sub, self._depth_sub, self._depth_info_sub), 10, 0.5)
+        self.get_logger().info("sync sub created")
+
+        self._synchronizer.registerCallback(self.pose_cb)
+        self.get_logger().info("sync callback created")
 
         return TransitionCallbackReturn.SUCCESS
 
@@ -108,54 +134,83 @@ class PosePublisher(LifecycleNode):
 
         return TransitionCallbackReturn.SUCCESS
 
-    #rgb callback
-    def rgb_cb(self, msg: image) -> None:
+    #Pose estimation callback 
+    def pose_cb(self, rgb_msg: Image, depth_msg: Image, depth_info: CameraInfo) :
 
-        poselist = PoseList() 
-        image_msg = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        self.rgb = cv2.cvtColor(cv2.flip(image_msg, 0), cv2.COLOR_BGR2RGB)                
-        
+        posearray_msg = PoseArray()
+        rgb_image_msg = self.cv_bridge.imgmsg_to_cv2(rgb_msg, "bgr8")
+        depth_image_msg = self.cv_bridge.imgmsg_to_cv2(depth_msg, "16UC1")
+
+        #flip image (camera mounted upside down)
+        rgb_image = cv2.cvtColor(cv2.flip(rgb_image_msg, 0), cv2.COLOR_BGR2RGB)
+
         with self.mp_pose.Pose(
                 min_detection_confidence=0.5,
                 min_tracking_confidence=0.5) as pose:
-                image.flags.writeable = False
-                results = pose.process(image)
+                rgb_image.flags.writeable = False
+                results = pose.process(rgb_image)
                 # Draw the pose annotation on the image.
-                image.flags.writeable = True
-                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                rgb_image.flags.writeable = True
+                image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
                 self.mp_drawing.draw_landmarks( image, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
                 h, w, c = image.shape
 
-                if results.pose_landmarks != None:
-                    index = 0
-                    for ids, pose_landmarks in enumerate(results.pose_landmarks.landmark):
-                        #check for wrist
-                        if 15 <= ids < 23:
-                            cx,cy = pose_landmarks.x*w, pose_landmarks.y*h
-                            poselist.human_pose[index].name = self.pose_name[ids]
-                            poselist.human_pose[index].x = cx
-                            poselist.human_pose[index].y = cy
-                            #poselist.human_pose[index].z = float(depth[int(cy),int(cx)])
-                            index+=1
-                    self._pose_pub.publish(poselist)
+                if results.pose_landmarks:
 
-                else: 
-                    index = 0
-                    for ids, pose_landmarks in enumerate(results.pose_landmarks.landmark):
-                        if 15 <= ids < 23:
-                            poselist.human_pose[index].name = self.pose_name[ids]
-                            poselist.human_pose[index].x = 0.0
-                            poselist.human_pose[index].y = 0.0
-                            #poselist.human_pose[index].z = 0.0
-                            index+=1
-                    self._pose_pub.publish(poselist)
+                    keypoints = self.process_results(results, depth_image_msg, depth_info , h, w) 
+                    posearray_msg.header =  rgb_msg.header 
+                    posearray_msg.pose = keypoints
 
-                img_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
+                self._pose_pub.publish(posearray_msg)
+                img_msg = self.cv_bridge.cv2_to_imgmsg(image, "bgr8")
                 self._im_pub.publish(img_msg)
+                
+    def process_results(self, results, depth_image_msg, depth_info, h, w):
+        keypoint_list = []
+        for ids, pose_landmarks in enumerate(results.pose_landmarks.landmark):
+            #check for wrist
+            if 15 <= ids < 23:
+                if pose_landmarks:
+                    keypoint_msg = Keypoint()
+                    det_x, det_y = pose_landmarks.x*w, pose_landmarks.y*h
+                    keypoint_msg.name = self.pose_name[ids]
+                    keypoint_msg.point2d.x = det_x
+                    keypoint_msg.point2d.y = det_y
+                    point = self.image_to_world(depth_image_msg, depth_info, int(det_x), int(det_y))
+                    keypoint_msg.point3d.x = point["x"]
+                    keypoint_msg.point3d.y = point["y"]
+                    keypoint_msg.point3d.z = point["z"]
 
-def main(args=None):
+                else:
+                    keypoint_msg = Keypoint()
+                    keypoint_msg.name = self.pose_name[ids]
+                    keypoint_msg.point2d = Point2D
+                    keypoint_msg.point3d = Point
 
-    rclpy.init(args=args)
+                keypoint_list.append(keypoint_msg)
+
+        return  keypoint_list
+
+    def image_to_world(self,depth_image_msg: np.ndarray,  depth_info: CameraInfo, center_x , center_y ) :
+        depth_image = cv2.flip(depth_image_msg, 0)
+        z = float(depth_image[center_y, center_x])
+        #Get camera intrinsic parameters
+        # Camera matrix K = [fx 0 cx]
+        #                   [0 fy cy]
+        #                   [0 0  1 ]
+
+        # project image to camera cordinates
+        k = depth_info.k
+        cx, cy, fx, fy = k[2], k[5], k[0], k[4]
+        x = z * (center_x - cx) / fx
+        y = z * (center_y - cy) / fy
+        point_list = {"x": x, "y": y, "z": z }
+
+        return point_list 
+
+
+def main():
+    rclpy.init()
     node=PosePublisher()
     node.trigger_configure()
     node.trigger_activate()
